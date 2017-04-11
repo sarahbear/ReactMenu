aaa6155c685b5126f76c5d6d646542d749ec1e97
sarahbear <nuriyejiao@gmail.com>
2017-04-11 09:10:48 +0800

    refactor(menu): style
    Change-Id: Ic7b2747d7d0ec1592f5217b8d66ee1ae779bf417

--- src/components/Menus/Menus.js
+++ src/components/Menus/Menus.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Menus.scss'
 
 class Menus extends React.Component {
 
@@ -18,15 +19,25 @@ class Menus extends React.Component {
 
     return (
       <div>
-        <ul key="sub1">
-          <li key="1"><input name="opt" type="checkbox" />Option 1</li>
-          <li key="2"><input name="opt" type="checkbox" />Option 2</li>
-          <li key="3"><input name="opt" type="checkbox" />Option 3</li>
-          <li key="4"><input name="opt" type="checkbox" />Option 4</li>
-        </ul>
+        <span>招聘职位</span>
+        <span>清空</span>
         <ul>
-          <li key="5"><input type="checkbox" />Option 5</li>
-          <li key="6"><input type="checkbox" />Option 6</li>
+          <li>
+            <input name="option" type="checkbox" />Option 1
+            <ul key="sub1">
+              <li key="1"><input name="opt" type="checkbox" />Option 1</li>
+              <li key="2"><input name="opt" type="checkbox" />Option 2</li>
+              <li key="3"><input name="opt" type="checkbox" />Option 3</li>
+              <li key="4"><input name="opt" type="checkbox" />Option 4</li>
+            </ul>
+          </li>
+          <li>
+            <input name="option" type="checkbox" />Option 1
+            <ul>
+              <li key="5"><input type="checkbox" />Option 5</li>
+              <li key="6"><input type="checkbox" />Option 6</li>
+            </ul>
+          </li>
         </ul>
       </div>
     );
