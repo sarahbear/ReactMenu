280202e70804b0979ee1abf4aa566548bae87427
sarahbear <nuriyejiao@gmail.com>
2017-04-13 19:30:17 +0800

    feature(menu):add subMenu component
    Change-Id: Ia6cc5783d94f7f53c2144dda83179fabbb2a3df5

--- src/components/Menus/Menus.js
+++ src/components/Menus/Menus.js
@@ -31,14 +31,7 @@ class Menus extends React.Component {
         <span className="menu-title">招聘职位</span>
         <a href="#" className="clear-button" onClick={this.onCheckAllChange}>清空</a>
         <ul>
-          <SubMenu  />
-          <li>
-            <input name="option" type="checkbox" />Option 1
-            <ul>
-              <li key="5"><input type="checkbox" />Option 5</li>
-              <li key="6"><input type="checkbox" />Option 6</li>
-            </ul>
-          </li>
+          {this.props.menus.map((subMenu, index) => <SubMenu key={index} subMenu={subMenu} /> )}
         </ul>
       </div>
     );
--- src/components/Menus/SubMenu.jsx
+++ src/components/Menus/SubMenu.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 
 class SubMenu extends React.Component {
 
-  render () {
+  handleClick = (e) => {
+    console.log(e.target.value);
+    if (e.target.value === 'on' ) {
+      
+    }
+  }
 
+  render () {
     return (
       <li>
-        <input name="option" type="checkbox" />Option 1
+        <input name="option" type="checkbox" onClick={this.handleClick} />{this.props.subMenu.label}
         <ul key="sub1">
-          <li key="1"><input name="opt" type="checkbox" />Option 1</li>
-          <li key="2"><input name="opt" type="checkbox" />Option 2</li>
-          <li key="3"><input name="opt" type="checkbox" />Option 3</li>
-          <li key="4"><input name="opt" type="checkbox" />Option 4</li>
+        {this.props.subMenu.children.map( (menuItem, index) => <li key={index}><input name="opt" type="checkbox" />{menuItem.label}</li> )}
         </ul>
       </li>
     );
